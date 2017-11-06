3f4217ff2ce529c691351804502cc76d700db87b
Toques <calvinwong8@yahoo.com>
2017-11-05 17:51:01 -0800

    Added content, style sheet, and js

---
+++ js/display.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#resumeButton").click(function(){
+        $("#resumeIFrame").toggle("slow");
+    });
+});
+
+function resumeClick(){
+	$("#resumeIFrame").toggle("slow");
+}
